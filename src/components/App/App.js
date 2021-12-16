62e57280b14dccbc93752665004de88a327f9e49
Brandon Jernigan <brandonkjernigan@gmail.com>
2021-12-16 13:13:53 -0700

    adjusted project structure, and added template sections for landing view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,27 @@
-import logo from '../../assets/logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import firebase from 'firebase/app';
 
-function App() {
+import Landing from '../../views/Landing/Landing';
+import logo from '../../assets/logo.svg';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyBAsoMYxzM46yavU-4Em-1ihbBNWzznvDg",
+    authDomain: "blueocean-612b2.firebaseapp.com",
+    projectId: "blueocean-612b2",
+    storageBucket: "blueocean-612b2.appspot.com",
+    messagingSenderId: "43711589559",
+    appId: "1:43711589559:web:effcb703d3262364feb132",
+    measurementId: "G-TW42PT564X"
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+  )
+};
 
 export default App;
---
+++ src/views/Landing/Landing.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './Landing.css';
+
+const Landing = () => {
+    return (
+        <div>
+            <div id={'login-section'} className={'section'}>
+                <h1 id={'login-title'}>Login...</h1>
+            </div>
+            <div id={'about-section'} className={'section'}>
+
+            </div>
+            <div id={'contact-section'} className={'section'}>
+
+            </div>
+        </div>
+    )
+};
+
+export default Landing;
