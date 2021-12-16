992f97d11d4dfcbabdaef1193610e4b32e87df55
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-16 15:09:25 -0600

    firebase config

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
 
 import Landing from '../../views/Landing/Landing';
 import logo from '../../assets/logo.svg';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyBAsoMYxzM46yavU-4Em-1ihbBNWzznvDg",
-    authDomain: "blueocean-612b2.firebaseapp.com",
-    projectId: "blueocean-612b2",
-    storageBucket: "blueocean-612b2.appspot.com",
-    messagingSenderId: "43711589559",
-    appId: "1:43711589559:web:effcb703d3262364feb132",
-    measurementId: "G-TW42PT564X"
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyBAsoMYxzM46yavU-4Em-1ihbBNWzznvDg",
+  authDomain: "blueocean-612b2.firebaseapp.com",
+  projectId: "blueocean-612b2",
+  storageBucket: "blueocean-612b2.appspot.com",
+  messagingSenderId: "43711589559",
+  appId: "1:43711589559:web:effcb703d3262364feb132",
+  measurementId: "G-TW42PT564X"
+};
+
 
 const App = () => {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-        </Routes>
-      </BrowserRouter>
+  initializeApp(firebaseConfig);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
   )
 };
 
