f5363a56bf66ffb165150ff5596caa158e059b74
yamagotsu <mipworld@yahoo.com>
2021-12-21 11:35:43 -0600

    new login

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,19 +1,51 @@
 import React from 'react';
-
 import './Landing.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import rough_draft from './rough_draft.png'
+import soldier_draft from './soldier_draft.png'
+
 
 const Landing = () => {
     return (
         <div>
             <div id={'login-section'} className={'section'}>
-                <h1 id={'login-title'}>Login...</h1>
-            </div>
-            <div id={'about-section'} className={'section'}>
+            
+            <div className='top-box'> 
+            <form className='top-form'>
+                <div class="mb-3">               
+
+                
+                    <h1>LOGIN</h1>               
+                                                            
+                    <label for="exampleInputEmail1" class="form-label" >Email:</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='ENTER YOUR EMAIL'/>
+                    <div></div> 
+                    <label for="exampleInputPassword1" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="ENTER YOUR PASSWORD"/></div>     
+                    
+                    <button type="submit" class="btn btn-outline-dark">Submit</button> 
+
+                    <button type="submit" class="btn btn-outline-dark" id='button2' >Forgot Password</button>                  
+                     
+                    <button type="submit" class="btn btn-outline-dark" id='button3'>New User</button>                      
+
+                    <img className='top-picture' src={soldier_draft} />                 
+                    
+             </form>  
+            
+             </div>               
+                </div>
+
+
+             {/* <div id={'about-section'} className={'section'}>
 
             </div>
+
+
             <div id={'contact-section'} className={'section'}>
 
-            </div>
+            </div>  */}
         </div>
     )
 };
