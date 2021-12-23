137d2214102410f8ac9b8ca35258c2674cde2fb1
yamagotsu <mipworld@yahoo.com>
2021-12-23 13:07:02 -0600

    card update

--- src/components/AdminPage/Admin.js
+++ src/components/AdminPage/Admin.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Admin.css";
 import Nav from "../../components/LeftNavBar/nav";
+import NewCard from "../../views/NewCard";
 
 export const Adminpage = () => {
   return (
     <>
       <Nav />
+      <NewCard />
       
     </>
   );
